deced721c9961cb0332f70a5aedeeb63e65287f4
Radosław Radzio <radeczkowo@gmail.com>
2022-03-22 21:01:19 +0100

    Version 1.2

--- Model.py
+++ Model.py
@@ -6,6 +6,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import AdaBoostClassifier
+from sklearn.linear_model import SGDClassifier
+from sklearn.linear_model import PassiveAggressiveClassifier
+from sklearn import svm
+from sklearn.gaussian_process import GaussianProcessClassifier
 from Functions import *
 
 submission_df = pd.read_csv("Data/sample_submission.csv", index_col=0).drop(columns=['Transported'])
@@ -18,29 +22,64 @@ df_train = sklearn.utils.shuffle(df_train)
 
 # RidgeClassifier
 
+print('RidgeClassifier:')
 model = RidgeClassifier()
 testmodel(df_train, model, 'Transported', 0.1)
 
 # RandomForestClassifier
 
+print('RandomForestClassifier:')
 model = RandomForestClassifier()
 testmodel(df_train, model, 'Transported', 0.1)
 
 # ExtraTreesClassifier
 
+print('ExtraTreesClassifier:')
 model = ExtraTreesClassifier()
 testmodel(df_train, model, 'Transported', 0.1)
 
 # DecisionTreeClassifier
 
+print('DecisionTreeClassifier:')
 model = DecisionTreeClassifier()
 testmodel(df_train, model, 'Transported', 0.1)
 
 # AdaBoostClassifier
 
+print('AdaBoostClassifier:')
 model = AdaBoostClassifier()
 testmodel(df_train, model, 'Transported', 0.1)
 
+# SGDClassifier
+
+print('SGDClassifier:')
+model = SGDClassifier()
+testmodel(df_train, model, 'Transported', 0.1)
+
+# PassiveAggressiveClassifier
+
+print('PassiveAggressiveClassifier:')
+model = PassiveAggressiveClassifier()
+testmodel(df_train, model, 'Transported', 0.1)
+
+# SVC
+
+print('SVC:')
+model = svm.SVC()
+testmodel(df_train, model, 'Transported', 0.1)
+
+# NuSVC
+
+print('NuSVC:')
+model = svm.NuSVC()
+testmodel(df_train, model, 'Transported', 0.1)
+
+# LinearSVC
+
+print('LinearSVC:')
+model = svm.LinearSVC()
+testmodel(df_train, model, 'Transported', 0.1)
+
 # Final model
-model = RandomForestClassifier()
+model = svm.SVC()
 finalprediction(model, df_train, df_test, 'Transported', submission_df)
--- Select_Data.py
+++ Select_Data.py
@@ -21,9 +21,9 @@ print(df_train.corr())
 # Using Extra Trees Classifier feature importance for dropping some less useful variables
 
 df_var_import = extratreefeatureselection(df_train.select_dtypes(include=np.number), 'Transported', 3)
-df_train, df_test = dropcolumnstesttrain(df_train, df_test, getnoimportantvar(df_var_import, 0.04))
+df_train, df_test = dropcolumnstesttrain(df_train, df_test, getnoimportantvar(df_var_import, 0.025))
 
-df_var_import = extratreefeatureselection(df_train.select_dtypes(include=np.number), 'Transported', 3)
+df_var_import = extratreefeatureselection(df_train.select_dtypes(include=np.number), 'Transported', 1)
 
 df_train.to_csv('Data/df_train', index=True)
 df_test.to_csv('Data/df_test', index=True)
